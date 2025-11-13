8c37bd748f1498a687c82e9489419dce0dbb3ae0
CodeMaestro13 <krishnanarde2001@gmail.com>
2025-11-13 13:48:20 +0530

    updated files

---
+++ Collections/CollectionFramework/src/collections/list/ArrayList1.java
@@ -0,0 +1,21 @@
+package collections.list;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        // collection do support the concept og methods:-
+        ArrayList al = new ArrayList<>();
+        al.add(100);
+        al.add(200);
+        al.add(3000);
+        al.add("krishna");
+        al.add(true);
+
+        System.out.println(al);
+
+        // this will follows all the characters of the ArrayList
+        // characters:--
+                // 1->
+    }
+}
